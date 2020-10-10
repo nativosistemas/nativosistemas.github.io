6aff85cb13a95337e9331f2c3388c6d0487ad591
nativosistemas <nativosistemas@outlook.com.ar>
2020-10-10 17:59:46 -0300

    react

---
+++ precache-manifest.c27a166251a3a41d6c4c915095a4b13f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f170f9f2b20528cd672225b8901d02a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1c9142b85744656f031",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d51d676a80320061982c",
+    "url": "/static/js/2.5d0dda9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5d0dda9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1c9142b85744656f031",
+    "url": "/static/js/main.c3307432.chunk.js"
+  },
+  {
+    "revision": "3035fb181b9acf4f4340",
+    "url": "/static/js/runtime-main.747519aa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
